a7a92d439b583af06ea8f7739778adedc7ee7e09
saurav0102 <32555426+saurav0102@users.noreply.github.com>
2023-03-01 12:03:13 -0600

    Updated Filter.ts
    Designed interface model for filter feature

--- Lunch-Decider-App/src/app/models/IFilter.ts
+++ Lunch-Decider-App/src/app/models/IFilter.ts
@@ -1,3 +1,12 @@
 export interface IFilter{
+    filterByFood: string;
+
+    filterByCuisine: string;
+
+    filterByOpenorClose: string;
+
+    filterByPrice:number;
+
+    filterByDistance: number;
     
 }
